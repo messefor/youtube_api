57ceb236f519fe142c3a2a5af818d0fade4066ca
Daisuke Oda <messefor@gmail.com>
2020-10-07 22:25:03 +0900

    add sheets api

--- src/example_api_key.py
+++ src/example_api_key.py
@@ -16,3 +16,24 @@ response = youtube.search().list(part='snippet',
                                 q=q,
                                 order='viewCount',
                                 type='video',).execute()
+
+# https://google-auth.readthedocs.io/en/latest/reference/google.oauth2.service_account.html
+# https://developers.google.com/sheets/api/quickstart/python
+# --------------------------------------------------------
+
+from google.oauth2 import service_account
+
+CREDENTIAL_FILE = 'config/youtube-data-api-291411-7aa5797d4450.json'
+credentials =\
+  service_account.Credentials.from_service_account_file(CREDENTIAL_FILE)
+
+
+SAMPLE_SPREADSHEET_ID = '1CL0pg_LDpASaHQqagDxBDKonmD64NRS6CQnpAbVDjhs'
+SAMPLE_RANGE_NAME = 'Class Data!A1:B4'
+
+service = build('sheets', 'v4', credentials=credentials)
+sheet = service.spreadsheets()
+
+result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                            range=SAMPLE_RANGE_NAME).execute()
+values = result.get('values', [])
